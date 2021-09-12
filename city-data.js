64250d7b90a5bf304631790f4f870bdf376c5b42
Taha Ebrahimzadeh <taha13800111@gmail.com>
2021-09-12 12:42:43 +0430

    Adding cities data

---
+++ city-data.js
@@ -0,0 +1,52 @@
+var data = [
+    {
+        id: 1,
+        lat: 35.6892,
+        lng: 51.3890,
+        time: 1502529980,
+        info: [{
+            key: 'name:',
+            value: 'Tehran',
+            name: "تهران"
+        }]
+    }, {
+        id: 2,
+        lat: 32.6539,
+        lng: 51.6660,
+        time: 1502759980,
+        info: [{
+            key: 'name:',
+            value: 'Isfahan',
+            name: "اصفهان"
+        }]
+    }, {
+        id: 3,
+        lat: 38.0962,
+        lng: 46.2738,
+        time: 1503079980,
+        info: [{
+            key: 'name:',
+            value: 'Tabriz',
+            name: "تبریز"
+        }]
+    }, {
+        id: 4,
+        lat: 36.2605,
+        lng: 59.6168,
+        time: 1503519980,
+        info: [{
+            key: 'name:',
+            value: 'Mashhad',
+            name: "مشهد"
+        }]
+    }, {
+        id: 5,
+        lat: 27.5300,
+        lng: 60.5821,
+        time: 1503939980,
+        info: [{
+            key: 'name:',
+            value: 'Sistan & Baluchestan',
+            name: "سیستان و بلوچستان"
+        }]
+    }]
